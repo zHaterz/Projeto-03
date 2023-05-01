8bd167bda1a7e1448cd490210d0c2140169f6409
Anderson <77762452+zHaterz@users.noreply.github.com>
2023-04-30 23:53:36 -0300

    Pequenas Alterações
    Mudanças

--- main.py
+++ main.py
@@ -1,42 +1,79 @@
-# por sequencia esse itens são Maçã,Ovo,Suco de caju, porem o cliente não tem a noção, que para conseguir esses alimento ele precisar por essa numeração, entao o jeito e deixa que ele escolha de forma intuitiva e o programa retorne o valor é a lista do que ele ira comer!
+# Criar uma calculadora com +,-, *, /
 
-# Correção de erros!
+from tkinter import *
+Calc = Tk()
 
-# Tkinter = Bom tentar introduzir o tkinter nas proximas linhas, a inteção e fazer uma janela funcional, com as opções pre-definidas para o usuario!
-
-# usando o tkinter, adicionar umas janela que peça o email é o numero telefonico, como forma de validação para sua "POSSIVEL" compra na loja de conveniencia 
-
-
-from mod import cole
-from mod import sms
-
-cole('Bem vindo a loja de conveniencia! :)')
-
-Alimentos = {'Alimentos_Saude' : 'Maçã' 'Uva' 'Alface' 'Tomate',
-'Alimentos_Gorduroso': 'Coxinha' 'Bacon' 'Ovo' 'Hamburg',  'Bebidas' : 'Suco de uva' 'Suco de Caju' 'Guarana' 'Coca-Cola'
-}
-
-
-saude = Alimentos['Alimentos_Saude']
-
-# ERRO: Bom até então aconteceu um erro no index da tupla, tentar corrigir! ou tratar o erro, bem provavel que a correção ajude mais para não acontecer futuramente, e a correção de erro ser menor
-cole('--Lista de Alimentos Saudaveis--')
-for n in range(0,2):
-    sms("Chata pra caralho!")
-
-# num = int(input('digite um numero: '))
-# if num == num:
-#     num1 = int(input('Deseja escolher mais um itens: '))
+# Funçoes
     
-#     c = num + 2
+class Calculadora:
+    def __init__(self):    
+        def soma(self,*num):
+            self.n = self.num
+            self.n+= self.n
+            # exibir["text"] = num
+            return self.n
+            
 
-# for t in range(num,c):
-#     c = num + 1
-#     sa = saude[t]
-#     cole(f'{sa}')
+        def sub(self,*num):
+            self.n = self.num
+            for self.c in self.n:
+                self.c = self.c - self.n
+                return self.c
+            
+
+        def mult(self,*num):
+            self.n = self.num
+            for self.c in self.n:
+                self.c = self.n * self.c
+                return self.c
+
+
+        def div(self,*num):
+            self.n = self.num
+            for self.c in self.n:
+                self.c = self.c // self.n
+                return self.c
+
+
+a = Calculadora()
+
+#   Programa 
+Calc.title("Calculadora")
+# Calc.geometry("50x50")
+
+#Botoes Numericos
+entra1 = Entry(Calc,width=20)
+entra1.grid(column=0, row=0)
+
+entra2 = Entry(Calc, width=20)
+entra2.grid(column=0, row=1)
+
     
 
+resul = Button(Calc, text="=")
+resul.grid(column=1, row=5)
 
+#Botoes Aritimeticos
+somas = Button(Calc, text="+", command= a.soma)
+somas.grid(column=0, row=2,padx=5, pady=5)
+
+dive = Button(Calc, text="/", command= a.div)
+dive.grid(column=1,row=2, padx=0, pady=5)
+
+multi = Button(Calc, text="X", command= a.mult)
+multi.grid(column=1, row=3, padx=5, pady=5)
+
+
+subr = Button(Calc, text="-", command= a.sub)
+subr.grid(column=0, row=3,padx=5, pady=5)
+
+
+# Mostra Tela 
+exibir = Label(Calc, text=f"Resultado...")
+exibir.grid(column=0, row=5)
+    
+    
+Calc.mainloop()
     
 
 
---
+++ test.py
@@ -0,0 +1,9 @@
+def conv(*txt):
+    num += txt
+    int(num)
+    return num
+
+n = "1"
+n2 = "2"
+
+conv(n, n2)
